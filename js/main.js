6dfcaa7cbf3255fd452939014661e6e142b10fc6
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-07-27 10:40:55 -0600

    Refactor to improve modularity, mantenibility and reusability

--- js/main.js
+++ js/main.js
@@ -111,155 +111,147 @@ const products = [
 
 let productsCart = [];
 
-//
 document.addEventListener("DOMContentLoaded", () => {
   renderProductList();
-  addEventListenersToAddToCartButtons();
   renderCartContent();
+  addGlobalEventListeners();
 });
 
 // Mapa para almacenar las referencias de los botones y contenedores
 const buttonReferences = new Map();
 
-function renderCartContent() {
-  const cartContent = document.getElementById("cart-content");
+// Constantes para selectores y otros valores
+const SELECTORS = {
+  cartContent: "#cart-content",
+  productList: "#product-list",
+  productListImage: ".product-image",
+  cartItemsCount: "#cart-items-count",
+  addToCartButton: ".btn-add-to-cart",
+  cartList: "#cart-list",
+  decrementCartButton: ".decrement-cart",
+  incrementCartButton: ".increment-cart",
+  addToCartItemCount: "#add-to-cart-count",
+};
 
+function renderProductList() {
+  const productsList = document.querySelector(SELECTORS.productList);
+  const productListItems = products.map(createProductListItem).join("");
+  productsList.innerHTML = productListItems;
+}
+
+function createProductListItem(product) {
+  return `
+    <li class="col-12 col-sm-6 col-lg-4" id="product-${product.id}">
+      <article>
+        <picture>
+          <source srcset="${
+            product.image.desktop
+          }" media="(min-width: 1200px)" />
+          <source srcset="${product.image.tablet}" media="(min-width: 768px)" />
+          <img src="${product.image.mobile}" alt="${
+    product.name
+  }" class="product-image img-fluid d-block rounded-3" />
+        </picture>
+        <div class="d-flex flex-column gap-1">
+          <button type="button" class="btn-add-to-cart btn border border-rose-light-300 bg-rose-light-50 rounded-5 px-4 py-2 text-nowrap w-75 d-block mx-auto translate-middle-y fw-semibold btn--product">
+            <img src="assets/images/icon-add-to-cart.svg" alt="" class="me-1" />
+            Add to Cart
+          </button>
+          <span class="text-rose-light-400">${product.category}</span>
+          <p class="fw-semibold text-rose-light-900 mb-0 text-nowrap">${
+            product.name
+          }</p>
+          <span class="fw-semibold text-sunset-red">${formatCurrency(
+            product.price
+          )}</span>
+        </div>
+      </article>
+    </li>`;
+}
+
+function renderCartContent() {
+  const cartContent = document.querySelector(SELECTORS.cartContent);
   if (productsCart.length === 0) {
-    cartContent.innerHTML = `
-      <img class="d-block mx-auto my-5" src="assets/images/illustration-empty-cart.svg" alt="" />
-      <p class="text-center fw-semibold text-rose-light-500 m-0">Your added items will appear here</p>`;
+    cartContent.innerHTML = getEmptyCartHTML();
   } else {
-    cartContent.innerHTML = `
-      <ul id="cart-list" class="p-0 list-unstyled"></ul>
-      <div class="d-flex justify-content-between align-items-center my-3">
-        <p style="font-size: 0.9rem" class="m-0 fw-light text-rose-light-900">Order Total</p>
-        <span id="cart-order-total" class="fs-5 fw-bold text-rose-light-900">
-          ${formatCurrency(
-            productsCart.reduce(
-              (acc, curr) => acc + curr.quantity * curr.price,
-              0
-            )
-          )}
-        </span>
-      </div>
-      <div class="bg-rose-light-100 d-flex justify-content-center align-items-center gap-2 p-3 rounded-3">
-        <img src="assets/images/icon-carbon-neutral.svg" alt="" />
-        <p style="font-size: 0.9rem" class="m-0 text-center">
-          This is a <span class="fw-semibold">carbon-neutral</span> delivery
-        </p>
-      </div>
-      <button data-bs-toggle="modal" data-bs-target="#order-modal" id="btn-confirm-order" class="btn rounded-5 btn-sunset-red w-100 mt-4 text-rose-light-100 fs-6 fw-lighter py-3 text-nowrap">
-        Confirm Order
-      </button>`;
+    cartContent.innerHTML = getCartContentHTML();
     renderCartItemsList();
   }
 }
 
-function renderProductList() {
-  const productsList = document.getElementById("product-list");
+function getEmptyCartHTML() {
+  return `
+    <img class="d-block mx-auto my-5" src="assets/images/illustration-empty-cart.svg" alt="" />
+    <p class="text-center fw-semibold text-rose-light-500 m-0">Your added items will appear here</p>`;
+}
 
-  const productListItems = products.map((product) => {
-    return `
-      <li class="col-12 col-sm-6 col-lg-4" id="product-${product.id}">
-        <article>
-          <picture>
-            <source srcset="${
-              product.image.desktop
-            }" media="(min-width: 1200px)" />
-            <source srcset="${
-              product.image.tablet
-            }" media="(min-width: 768px)" />
-            <img src="${product.image.mobile}" alt="${
-      product.name
-    }" class="img-fluid d-block rounded-3" />
-          </picture>
-          <div class="d-flex flex-column gap-1">
-            <button type="button" class="btn-add-to-cart btn border border-rose-light-300 bg-rose-light-50 rounded-5 px-4 py-2 text-nowrap w-75 d-block mx-auto translate-middle-y fw-semibold btn--product">
-              <img src="assets/images/icon-add-to-cart.svg" alt="" class="me-1" />
-              Add to Cart
-            </button>
-            <span class="text-rose-light-400">${product.category}</span>
-            <p class="fw-semibold text-rose-light-900 mb-0 text-nowrap">${
-              product.name
-            }</p>
-            <span class="fw-semibold text-sunset-red">${formatCurrency(
-              product.price
-            )}</span>
-          </div>
-        </article>
-      </li>`;
-  });
-
-  productsList.innerHTML = productListItems.join("");
+function getCartContentHTML() {
+  return `
+    <ul id="cart-list" class="p-0 list-unstyled"></ul>
+    <div class="d-flex justify-content-between align-items-center my-3">
+      <p style="font-size: 0.9rem" class="m-0 fw-light text-rose-light-900">Order Total</p>
+      <span id="cart-order-total" class="fs-5 fw-bold text-rose-light-900">
+        ${formatCurrency(calculateOrderTotal())}
+      </span>
+    </div>
+    <div class="bg-rose-light-100 d-flex justify-content-center align-items-center gap-2 p-3 rounded-3">
+      <img src="assets/images/icon-carbon-neutral.svg" alt="" />
+      <p style="font-size: 0.9rem" class="m-0 text-center">
+        This is a <span class="fw-semibold">carbon-neutral</span> delivery
+      </p>
+    </div>
+    <button data-bs-toggle="modal" data-bs-target="#order-modal" id="btn-confirm-order" class="btn rounded-5 btn-sunset-red w-100 mt-4 text-rose-light-100 fs-6 fw-lighter py-3 text-nowrap">
+      Confirm Order
+    </button>`;
 }
 
 function renderCartItemsList() {
-  const cartItemsList = document.getElementById("cart-list");
+  const cartItemsList = document.querySelector(SELECTORS.cartList);
+  const cartItems = productsCart.map(createCartItemHTML).join("");
+  cartItemsList.innerHTML = cartItems;
+}
 
-  const cartItems = productsCart.map((product) => {
-    return `
-      <li class="d-flex align-items-center justify-content-between border-bottom py-2">
-        <div class="d-flex flex-column gap-1">
-          <p class="fw-semibold m-0">${product.name}</p>
-          <div class="d-flex gap-2">
-            <span class="text-sunset-red fw-semibold">${
-              product.quantity
-            }x</span>
-            <span class="fw-light text-rose-light-500">@ ${formatCurrency(
-              product.price
-            )}</span>
-            <span class="fw-semibold text-rose-light-500">${formatCurrency(
-              product.quantity * product.price
-            )}</span>
-          </div>
+function createCartItemHTML(product) {
+  return `
+    <li class="d-flex align-items-center justify-content-between border-bottom py-2">
+      <div class="d-flex flex-column gap-1">
+        <p class="fw-semibold m-0">${product.name}</p>
+        <div class="d-flex gap-2">
+          <span class="text-sunset-red fw-semibold">${product.quantity}x</span>
+          <span class="fw-light text-rose-light-500">@ ${formatCurrency(
+            product.price
+          )}</span>
+          <span class="fw-semibold text-rose-light-500">${formatCurrency(
+            product.quantity * product.price
+          )}</span>
         </div>
-        <button onclick='removeItemFromCart(${
-          product.id
-        })' class="btn-icon btn-icon--cart rounded-circle d-flex justify-content-center align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
-            <path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
-          </svg>
-        </button>
-      </li>`;
-  });
-
-  cartItemsList.innerHTML = cartItems.join("");
+      </div>
+      <button onclick='removeItemFromCart(${
+        product.id
+      })' class="btn-icon btn-icon--cart rounded-circle d-flex justify-content-center align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+          <path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
+        </svg>
+      </button>
+    </li>`;
 }
 
-function removeItemFromCart(itemId) {
-  productsCart = productsCart.filter((p) => p.id !== itemId);
-  renderCartContent();
-  document.getElementById("cart-items-count").textContent = productsCart.reduce(
-    (acc, curr) => acc + curr.quantity,
-    0
-  );
-
-  // Restaurar el botón "Add to Cart" y remover el contador del producto
-  const { addToCartButton, addToCartButtonContainer, cartItemCountUpdater } =
-    buttonReferences.get(itemId);
-  addToCartButtonContainer.prepend(addToCartButton);
-  cartItemCountUpdater.remove();
+function addGlobalEventListeners() {
+  document.addEventListener("click", handleGlobalClick);
 }
 
-function formatCurrency(currency) {
-  return currency.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+function handleGlobalClick(event) {
+  const addToCartButton = event.target.closest(SELECTORS.addToCartButton);
+  if (addToCartButton) {
+    handleAddToCart(addToCartButton);
+  }
 }
 
-function addEventListenersToAddToCartButtons() {
-  const buttonsAddToCart = document.querySelectorAll(".btn-add-to-cart");
-  buttonsAddToCart.forEach((button) =>
-    button.addEventListener("click", handleAddToCart)
-  );
-}
-
-function handleAddToCart(e) {
-  const addToCartButton = e.target.closest(".btn-add-to-cart");
+function handleAddToCart(addToCartButton) {
   const productListItem = addToCartButton.closest("li");
+  const productImage = productListItem.querySelector(
+    SELECTORS.productListImage
+  );
   const productId = parseInt(productListItem.getAttribute("id").split("-")[1]);
   const addToCartButtonContainer = addToCartButton.parentElement;
 
@@ -276,8 +268,10 @@ function handleAddToCart(e) {
     addToCartButton,
     addToCartButtonContainer,
     cartItemCountUpdater,
+    productImage,
   });
 
+  productImage.classList.add("product-image-bordered");
   addToCartButton.remove();
   addToCartButtonContainer.prepend(cartItemCountUpdater);
 }
@@ -287,33 +281,24 @@ function updateCart(productId) {
     (product) => product.id === productId
   );
 
-  const product = products.find((p) => p.id === productId);
-
   if (productInCart) {
     productInCart.quantity += 1;
   } else {
-    const newProduct = {
+    const product = products.find((p) => p.id === productId);
+
+    productsCart.push({
       id: productId,
       name: product.name,
       quantity: 1,
       price: product.price,
-    };
-    productsCart = [...productsCart, newProduct];
+    });
   }
 
-  document.getElementById("cart-items-count").textContent = productsCart.reduce(
-    (acc, curr) => acc + curr.quantity,
-    0
-  );
-
+  updateCartCount();
   renderCartContent();
 }
 
-function createCartItemCountUpdater(
-  productId,
-  addToCartButton,
-  addToCartButtonContainer
-) {
+function createCartItemCountUpdater(productId) {
   const cartItemCountUpdater = document.createElement("div");
   cartItemCountUpdater.setAttribute(
     "class",
@@ -323,84 +308,108 @@ function createCartItemCountUpdater(
   cartItemCountUpdater.innerHTML =
     "<button class='decrement-cart btn-icon btn-icon--product'><svg xmlns='http://www.w3.org/2000/svg' width='10' height='2' fill='none' viewBox='0 0 10 2'><path d='M0 .375h10v1.25H0V.375Z'/></svg></button><span id='add-to-cart-count'>1</span><button class='increment-cart btn-icon btn-icon--product'><svg xmlns='http://www.w3.org/2000/svg' width='10' height='10' fill='none' viewBox='0 0 10 10'><path d='M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z'/></svg></button>";
 
-  addEventListenersToUpdateCartButtons(
-    cartItemCountUpdater,
-    productId,
-    addToCartButton,
-    addToCartButtonContainer
-  );
+  addEventListenersToUpdateCartButtons(cartItemCountUpdater, productId);
 
   return cartItemCountUpdater;
 }
 
-function addEventListenersToUpdateCartButtons(
-  cartItemCountUpdater,
-  productId,
-  addToCartButton,
-  addToCartButtonContainer
-) {
+function addEventListenersToUpdateCartButtons(cartItemCountUpdater, productId) {
   cartItemCountUpdater
-    .querySelector(".decrement-cart")
+    .querySelector(SELECTORS.decrementCartButton)
     .addEventListener("click", () => {
-      handleDecrementCart(
-        productId,
-        addToCartButton,
-        addToCartButtonContainer,
-        cartItemCountUpdater
-      );
+      handleDecrementCart(productId);
     });
 
   cartItemCountUpdater
-    .querySelector(".increment-cart")
+    .querySelector(SELECTORS.incrementCartButton)
     .addEventListener("click", () => {
-      handleIncrementCart(productId, cartItemCountUpdater);
+      handleIncrementCart(productId);
     });
 }
 
-function handleDecrementCart(
-  productId,
-  addToCartButton,
-  addToCartButtonContainer,
-  cartItemCountUpdater
-) {
+function handleDecrementCart(productId) {
+  const {
+    addToCartButton,
+    addToCartButtonContainer,
+    cartItemCountUpdater,
+    productImage,
+  } = buttonReferences.get(productId);
+
   productsCart = productsCart
     .map((product) => {
       if (product.id == productId) {
         const updatedQuantity = product.quantity - 1;
-        cartItemCountUpdater.querySelector("#add-to-cart-count").textContent =
-          updatedQuantity;
+        cartItemCountUpdater.querySelector(
+          SELECTORS.addToCartItemCount
+        ).textContent = updatedQuantity;
         return { ...product, quantity: updatedQuantity };
-      } else return product;
+      }
+      return product;
     })
     .filter((product) => product.quantity > 0);
 
   if (!productsCart.some((product) => product.id === productId)) {
+    productImage.classList.remove("product-image-bordered");
     addToCartButtonContainer.prepend(addToCartButton);
     cartItemCountUpdater.remove();
   }
 
-  document.getElementById("cart-items-count").textContent = productsCart.reduce(
-    (acc, curr) => acc + curr.quantity,
-    0
-  );
-
+  updateCartCount();
   renderCartContent();
 }
 
-function handleIncrementCart(productId, cartItemCountUpdater) {
+function handleIncrementCart(productId) {
   productsCart = productsCart.map((product) => {
     if (product.id == productId) {
       const updatedQuantity = product.quantity + 1;
-      cartItemCountUpdater.querySelector("#add-to-cart-count").textContent =
-        updatedQuantity;
+      document.querySelector(
+        `#product-${productId} ${SELECTORS.addToCartItemCount}`
+      ).textContent = updatedQuantity;
       return { ...product, quantity: updatedQuantity };
-    } else return product;
+    }
+    return product;
   });
 
-  document.getElementById("cart-items-count").textContent = productsCart.reduce(
-    (acc, curr) => acc + curr.quantity,
+  updateCartCount();
+  renderCartContent();
+}
+
+function updateCartCount() {
+  document.querySelector(SELECTORS.cartItemsCount).textContent =
+    productsCart.reduce((acc, curr) => acc + curr.quantity, 0);
+}
+
+function removeItemFromCart(productId) {
+  productsCart = productsCart.filter((product) => product.id !== productId);
+  updateCartCount();
+  renderCartContent();
+
+  const {
+    addToCartButton,
+    addToCartButtonContainer,
+    cartItemCountUpdater,
+    productImage,
+  } = buttonReferences.get(productId);
+
+  if (addToCartButton && addToCartButtonContainer) {
+    addToCartButtonContainer.prepend(addToCartButton);
+    if (cartItemCountUpdater) {
+      productImage.classList.remove("product-image-bordered");
+      cartItemCountUpdater.remove();
+    }
+  }
+}
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function calculateOrderTotal() {
+  return productsCart.reduce(
+    (total, product) => total + product.price * product.quantity,
     0
   );
-
-  renderCartContent();
 }
