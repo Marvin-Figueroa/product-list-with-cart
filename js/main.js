0e75afb7a6117ad1dad600575998f29544739fe7
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-07-27 20:25:31 -0600

    Add cart order modal

--- js/main.js
+++ js/main.js
@@ -111,16 +111,8 @@ const products = [
 
 let productsCart = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderProductList();
-  renderCartContent();
-  addGlobalEventListeners();
-});
-
-// Mapa para almacenar las referencias de los botones y contenedores
 const buttonReferences = new Map();
 
-// Constantes para selectores y otros valores
 const SELECTORS = {
   cartContent: "#cart-content",
   productList: "#product-list",
@@ -131,7 +123,18 @@ const SELECTORS = {
   decrementCartButton: ".decrement-cart",
   incrementCartButton: ".increment-cart",
   addToCartItemCount: "#add-to-cart-count",
+  orderModalCartContent: "#order-modal-content",
+  confirmOrderButton: "#btn-confirm-order",
+  orderModal: "#order-modal",
+  startNewOrderButton: "#start-new-order-btn",
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProductList();
+  renderCartContent();
+  addGlobalEventListeners();
+  createOrderModal();
+});
 
 function renderProductList() {
   const productsList = document.querySelector(SELECTORS.productList);
@@ -200,7 +203,7 @@ function getCartContentHTML() {
         This is a <span class="fw-semibold">carbon-neutral</span> delivery
       </p>
     </div>
-    <button data-bs-toggle="modal" data-bs-target="#order-modal" id="btn-confirm-order" class="btn rounded-5 btn-sunset-red w-100 mt-4 text-rose-light-100 fs-6 fw-lighter py-3 text-nowrap">
+    <button data-bs-toggle="modal" data-bs-target="#order-modal" id="btn-confirm-order" class="btn rounded-5 btn-sunset-red w-100 mt-4 text-rose-light-100 fs-6 fw-lighter py-2 text-nowrap">
       Confirm Order
     </button>`;
 }
@@ -209,6 +212,50 @@ function renderCartItemsList() {
   const cartItemsList = document.querySelector(SELECTORS.cartList);
   const cartItems = productsCart.map(createCartItemHTML).join("");
   cartItemsList.innerHTML = cartItems;
+}
+
+function renderModalCartItemsList() {
+  const modalCartContent = document.querySelector(
+    SELECTORS.orderModalCartContent
+  );
+
+  if (modalCartContent) {
+    const modalCartItems = productsCart.map(createModalCartItemHTML).join("");
+    modalCartContent.innerHTML =
+      modalCartItems +
+      `<div class="d-flex justify-content-between align-items-center my-3">
+      <p style="font-size: 0.9rem" class="m-0 fw-light text-rose-light-900">Order Total</p>
+      <span id="cart-order-total" class="fs-5 fw-bold text-rose-light-900">
+        ${formatCurrency(calculateOrderTotal())}
+      </span>
+    </div>`;
+  } else {
+    console.error("Order modal cart content element not found");
+  }
+}
+
+function createModalCartItemHTML(product) {
+  return `
+      <li class="d-flex align-items-center gap-4 border-bottom py-3">
+          <img class='modal__product-image' src='${product.thumbnail}' alt='${
+    product.name
+  }'>
+          <div class="d-flex flex-column gap-1">
+              <p class="fw-semibold m-0">${product.name}</p>
+              <div class="d-flex gap-2">
+                  <span class="text-sunset-red fw-semibold">${
+                    product.quantity
+                  }x</span>
+                  <span class="fw-light text-rose-light-500 justify-selft-end">@ ${formatCurrency(
+                    product.price
+                  )}</span>
+              </div>
+          </div>
+          <span class="fw-semibold text-rose-light-900 ms-auto">${formatCurrency(
+            product.quantity * product.price
+          )}</span>
+      </li>
+  `;
 }
 
 function createCartItemHTML(product) {
@@ -236,6 +283,27 @@ function createCartItemHTML(product) {
     </li>`;
 }
 
+function createOrderModal() {
+  const modalHTML = `
+      <div class="modal fade" id="order-modal" tabindex="-1" aria-labelledby="order-modal-label" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content p-4">
+                  <svg class="mb-4" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M21 32.121L13.5 24.6195L15.6195 22.5L21 27.879L32.3775 16.5L34.5 18.6225L21 32.121Z" fill="#1EA575"/>
+                      <path d="M24 3C19.8466 3 15.7865 4.23163 12.333 6.53914C8.8796 8.84665 6.18798 12.1264 4.59854 15.9636C3.0091 19.8009 2.59323 24.0233 3.40352 28.0969C4.21381 32.1705 6.21386 35.9123 9.15077 38.8492C12.0877 41.7861 15.8295 43.7862 19.9031 44.5965C23.9767 45.4068 28.1991 44.9909 32.0364 43.4015C35.8736 41.812 39.1534 39.1204 41.4609 35.667C43.7684 32.2135 45 28.1534 45 24C45 18.4305 42.7875 13.089 38.8493 9.15076C34.911 5.21249 29.5696 3 24 3ZM24 42C20.4399 42 16.9598 40.9443 13.9997 38.9665C11.0397 36.9886 8.73256 34.1774 7.37018 30.8883C6.0078 27.5992 5.65134 23.98 6.34587 20.4884C7.04041 16.9967 8.75474 13.7894 11.2721 11.2721C13.7894 8.75473 16.9967 7.0404 20.4884 6.34587C23.98 5.65133 27.5992 6.00779 30.8883 7.37017C34.1774 8.73255 36.9886 11.0397 38.9665 13.9997C40.9443 16.9598 42 20.4399 42 24C42 28.7739 40.1036 33.3523 36.7279 36.7279C33.3523 40.1036 28.7739 42 24 42Z" fill="#1EA575"/>
+                  </svg>
+                  <h1 class="modal-title fs-2 text-rose-light-900 fw-bold m-0 mb-1 p-0" id="order-modal-label">Order Confirmed</h1>
+                  <p class="fs-6 text-rose-light-500 m-0 mb-4 p-0">We hope you enjoy your food!</p>
+                  <div id="order-modal-content" class="bg-rose-light-100 rounded px-3"></div>
+                  <button id='start-new-order-btn' type="button" class="btn rounded-5 py-2 mt-4 btn-sunset-red text-capitalize w-100" data-bs-dismiss="modal">start new order</button>
+              </div>
+          </div>
+      </div>
+  `;
+
+  document.body.insertAdjacentHTML("beforeend", modalHTML);
+}
+
 function addGlobalEventListeners() {
   document.addEventListener("click", handleGlobalClick);
 }
@@ -244,6 +312,22 @@ function handleGlobalClick(event) {
   const addToCartButton = event.target.closest(SELECTORS.addToCartButton);
   if (addToCartButton) {
     handleAddToCart(addToCartButton);
+  }
+
+  const confirmOrderButton = event.target.closest(SELECTORS.confirmOrderButton);
+  if (confirmOrderButton) {
+    createOrderModal();
+    renderModalCartItemsList();
+  }
+
+  const startNewOrderButton = event.target.closest(
+    SELECTORS.startNewOrderButton
+  );
+  if (startNewOrderButton) {
+    productsCart = [];
+    updateCartCount();
+    renderCartContent();
+    renderProductList();
   }
 }
 
@@ -291,6 +375,7 @@ function updateCart(productId) {
       name: product.name,
       quantity: 1,
       price: product.price,
+      thumbnail: product.image.thumbnail,
     });
   }
 
